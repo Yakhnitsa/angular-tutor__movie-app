ba58ca9ad7f7fba9760167a760e3238409610d31
Yakhnitsa <transpmailbox@gmail.com>
2020-08-03 07:07:00 +0300

    Init base application components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
+import { MainBarComponent } from './main-bar/main-bar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieCardComponent,
+    BottomBarComponent,
+    MainBarComponent,
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
